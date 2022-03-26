e15bff9455694adf4475983ac7239a7b99b7ee9d
Mona Lisa <you@example.com>
2022-03-26 17:44:57 +0100

    44

--- src/views/Farms/components/FarmCard/CardActionsContainer.tsx
+++ src/views/Farms/components/FarmCard/CardActionsContainer.tsx
@@ -14,42 +14,11 @@ import HarvestAction from './HarvestAction'
 
 const Action = styled.div`
   padding-top: 16px;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  background-color: rgba(255,255,255,0.2);
-  padding-left: 10px;
-  padding-bottom: 10px;
-  border-radius: 16px;
-  height: 100%;
-
-  ${({ theme }) => theme.mediaQueries.xs} {
-    & #include {
-      grid-column:  span 2;
-      margin-left: 0px;
-    }
-  }
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    & #include {
-      grid-column:  span 1;
-      margin-left: 0px;
-    }
-  }
-
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    & #include {
-      grid-column: span 1;
-    }
-  }
-  
 `
 export interface FarmWithStakedValue extends Farm {
   apy?: BigNumber
 }
-const ExpandingWrapper2 = styled.div`
-  margin-right: 20px;
-`
+
 interface FarmCardActionsProps {
   farm: FarmWithStakedValue
   ethereum?: provider
@@ -85,18 +54,9 @@ const CardActions: React.FC<FarmCardActionsProps> = ({ farm, ethereum, account }
     }
   }, [onApprove])
 
-  let newBalance
-  if (pid === 11 || pid === 10) {
-    newBalance = stakedBalance.times(10 ** 12)
-  } else if (pid === 14) {
-    newBalance = stakedBalance.times(10 ** 10)
-  } else {
-    newBalance = stakedBalance
-  }
-
   const renderApprovalOrStakeButton = () => {
     return isApproved ? (
-      <StakeAction stakedBalance={newBalance} tokenBalance={tokenBalance} tokenName={lpName} pid={pid} depositFeeBP={depositFeeBP} />
+      <StakeAction stakedBalance={stakedBalance} tokenBalance={tokenBalance} tokenName={lpName} pid={pid} depositFeeBP={depositFeeBP} />
     ) : (
       <Button mt="8px" fullWidth disabled={requestedApproval} onClick={handleApprove}>
         {TranslateString(999, 'Approve Contract')}
@@ -106,19 +66,16 @@ const CardActions: React.FC<FarmCardActionsProps> = ({ farm, ethereum, account }
 
   return (
     <Action>
-      <ExpandingWrapper2 id="include">
       <Flex>
         <Text bold textTransform="uppercase" color="secondary" fontSize="12px" pr="3px">
           {/* TODO: Is there a way to get a dynamic value here from useFarmFromSymbol? */}
-          COAL
+          PORTAL
         </Text>
         <Text bold textTransform="uppercase" color="textSubtle" fontSize="12px">
           {TranslateString(999, 'Earned')}
         </Text>
       </Flex>
-      <HarvestAction  earnings={earnings} pid={pid} />
-      </ExpandingWrapper2>
-      <ExpandingWrapper2 id="include">
+      <HarvestAction earnings={earnings} pid={pid} />
       <Flex>
         <Text bold textTransform="uppercase" color="secondary" fontSize="12px" pr="3px">
           {lpName}
@@ -128,7 +85,6 @@ const CardActions: React.FC<FarmCardActionsProps> = ({ farm, ethereum, account }
         </Text>
       </Flex>
       {!account ? <UnlockButton mt="8px" fullWidth /> : renderApprovalOrStakeButton()}
-      </ExpandingWrapper2>
     </Action>
   )
 }
